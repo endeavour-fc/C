0d1ff24f1e96f39fd9749adb1b5e2e928db98513
fengchao <1669128906@qq.com>
2019-12-11 21:42:09 +0800

    提交代码

--- 析构函数/static的操作/static_practice.cpp
+++ 析构函数/static的操作/static_practice.cpp
@@ -11,6 +11,8 @@ public:
 		strcpy(p, n);
 		score = sco;
 	}
+	friend ostream& operator<<(ostream& os, Stu s1);
+	friend ostream& operator>>(ostream& is, Stu s1);
 	~Stu()
 	{
 		if (p != NULL)
@@ -30,16 +32,30 @@ private:
 	int score;
 	int average;
 };
-
+ostream& operator<<(ostream& os, Stu s1)
+{
+	if (s1.p != NULL)
+	{
+		os << "姓名是" << s1.p << endl;
+		os << "成绩是" << s1.score << endl;
+		os << "分数是" << s1.average << endl;
+	}
+	return os;
+}
+//ostream& operator>>(ostream& is, Stu s1)
+//{
+//
+//}
 int main()
 {
 	int a = 100;
 	char q[20] = "fengchao";
 	Stu s1(a,q);
-	s1.printStu();
+	//s1.printStu();
+	cout << s1 << endl;
 	
 	int b = 100;
 	char c[20] = "xiaokk";
 	Stu s2(b, c);
-	s2.printStu();
+	cout << s1 << endl;
 }
