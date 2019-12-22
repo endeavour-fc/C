17fa0282a5efdacdc57a913ee4f949a09bf8114e
fengchao <1669128906@qq.com>
2019-12-22 21:58:31 +0800

    "C"

--- practice_8_13/practice_8_13/practice.cpp
+++ practice_8_13/practice_8_13/practice.cpp
@@ -486,64 +486,64 @@ int taddOK(int x, int y) {
 //	temp->next = temp->next->next;
 //	return pCur;
 //}
-#include<iostream>
-#include<string.h>
-using namespace std;
-class MyString
-{
- public:
-  MyString()
-       {
-            this->str = NULL;
-            this->len = 0;
-        }
-        MyString(const char* str)
-         {
-           this->len = strlen(str);
-           this->str = new char[len + 1];
-            strcpy(this->str, str);
-         }
-         MyString operator+(MyString& another)
-       {
-			 MyString temp;
-            temp.len = another.len + this->len;
-			temp.str = new char[another.len + this->len + 1];
-		strcpy(temp.str, this->str);
-		strcat(temp.str, another.str);
-            return temp;
-         }
-		 MyString& operator=(const MyString& another)
-		 {
-			 if (strcmp(this->str, another.str) == 0)
-			 {
-				 return *this;
-			 }
-			 if (this->str == NULL)
-			 {
-				 this->len = another.len;
-				 this->str = new char[this->len + 1];
-				 strcpy(this->str, another.str);
-				 return *this;
-			 }
-			 else
-             {
-                 delete[]this->str;
-                 this->str = new char[another.len + 1];
-                 this->len = another.len;
-                 strcpy(this->str, another.str);
-                 return *this;
-
-             }
-
-         }
-		 char* GetString()
-         {
-             return this->str;
-         }
-     private:
-         char* str;
-         int len;
- };
+//#include<iostream>
+//#include<string.h>
+//using namespace std;
+//class MyString
+//{
+// public:
+//  MyString()
+//       {
+//            this->str = NULL;
+//            this->len = 0;
+//        }
+//        MyString(const char* str)
+//         {
+//           this->len = strlen(str);
+//           this->str = new char[len + 1];
+//            strcpy(this->str, str);
+//         }
+//         MyString operator+(MyString& another)
+//       {
+//			 MyString temp;
+//            temp.len = another.len + this->len;
+//			temp.str = new char[another.len + this->len + 1];
+//		strcpy(temp.str, this->str);
+//		strcat(temp.str, another.str);
+//            return temp;
+//         }
+//		 MyString& operator=(const MyString& another)
+//		 {
+//			 if (strcmp(this->str, another.str) == 0)
+//			 {
+//				 return *this;
+//			 }
+//			 if (this->str == NULL)
+//			 {
+//				 this->len = another.len;
+//				 this->str = new char[this->len + 1];
+//				 strcpy(this->str, another.str);
+//				 return *this;
+//			 }
+//			 else
+//             {
+//                 delete[]this->str;
+//                 this->str = new char[another.len + 1];
+//                 this->len = another.len;
+//                 strcpy(this->str, another.str);
+//                 return *this;
+//
+//             }
+//
+//         }
+//		 char* GetString()
+//         {
+//             return this->str;
+//         }
+//     private:
+//         char* str;
+//         int len;
+// };
 //int main()
 //{
 //     MyString s1("hhhhhh");
