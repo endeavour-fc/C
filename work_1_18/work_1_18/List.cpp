e7455171e4f6f14fb21fd2a340cfada32e8efb18
fengchao <1669128906@qq.com>
2020-02-09 18:57:29 +0800

    "合并两个数组"

--- work_1_18/work_1_18/List.cpp
+++ work_1_18/work_1_18/List.cpp
@@ -638,6 +638,30 @@ void sort(int* arr, int len)
 	}
 	
 }
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+
+	int* temp = (int*)malloc(sizeof(int)*(m + n));
+	int n1 = 0;
+	int n2 = 0;
+	int i = 0;
+	while (n1<m&&n2<n)
+	{
+		if (nums1[n1]<nums2[n2])
+		{
+			temp[i++] = nums1[n1++];
+		}
+		else
+		{
+			temp[i++] = nums2[n2++];
+		}
+	}
+	while (n1<m)
+		temp[i++] = nums1[n1++];
+	while (n2<n)
+		temp[i++] = nums2[n2++];
+	for (int i = 0; i<m + n; i++)
+		nums1[i] = temp[i];
+}
 int main()
 {
 	char str[] = " ";
