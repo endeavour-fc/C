36c2ee0b1cb8ad77b184b0b4c9815b0cf3942f9a
fengchao <1669128906@qq.com>
2020-02-16 16:04:23 +0800

    "string类的实现"

--- work_2_12/work_2_12/delete_or_new.cpp
+++ work_2_12/work_2_12/delete_or_new.cpp
@@ -64,6 +64,7 @@ public:
 		str = new char[strlen(temp)*2 + 1];
 		this->capacity = this->len * 2 + 1;
 		strcpy(str, temp);
+		str[len + 1] = '\0';
 	}
 	String()
 	{
@@ -72,46 +73,86 @@ public:
 		*(this->str) = '\0';
 		this->capacity = 1;
 	}
-	String(String& another)
+	String(const String& another)
 	{
 		this->str = new char[another.len * 2 + 1];
 		this->len = another.len;
-		this->capacity = this->len * 2 + 1;
-		strcpy(str, another.str);
+		this->capacity = another.capacity;
+		strncpy(str, another.str,this->len);
+		str[len + 1] = '\0';
 	}
 	~String()
-	{}
+	{
+		if (str)
+		{
+			delete[] str;
+			str = NULL;
+		}
+	}
 	String&operator+=(String& another)
 	{
-		if(this->len + another.len > this->capacity)
+		if(this->len + another.len >= this->capacity)
 		{
 			this->str = (char*)realloc(this->str, this->len + another.len + 10);
 			this->capacity = this->len + 1 + 10;
 		}
-		this->len = another.len+this->len;
-		strcat(this->str, another.str);
+		strncat(this->str, another.str,another.len);
+		this->len = another.len + this->len;
+		str[len + 1] = '\0';
 		return *this;
 	}
 	String&operator+=(const char* temp)
 	{
-		if(this->len + strlen(temp) > this->capacity)
+		if(this->len + strlen(temp) >= this->capacity)
 		{
 			this->str = (char*)realloc(this->str, this->len + strlen(temp) + 10);
 			this->capacity = this->len + 1 + 10;
 		}
 		this->len += strlen(temp);
 		strcat(this->str, temp);
+		str[len + 1] = '\0';
 		return *this;
 	}
 	String&operator+=(const char temp)
 	{
-		if(this->len + 1 > this->capacity)
+		if(this->len + 1 >= this->capacity)
 		{
 			this->str = (char*)realloc(this->str, this->len + 1 + 10);
 			this->capacity = this->len + 1 + 10;
 		}
 		this->str[len] = temp;
 		this->len += 1;
+		str[len + 1] = '\0';
+		return *this;
+	}
+	//String&operator=(const String& another)
+	//{
+	//	if (this != &another)
+	//	{
+	//		if (this->capacity < another.len)
+	//		{
+	//			delete[] this->str;
+	//			this->str = new char[another.len * 2 + 1];
+	//			this->len = another.len;
+	//			this->capacity = another.len * 2 + 1;
+	//		}
+	//		strncpy(this->str, another.str, another.len);
+	//	}
+	//	return *this;
+	//}
+	void Swap(String& s2)
+	{
+		std::swap(str, s2.str);
+		std::swap(capacity, s2.capacity);
+		std::swap(len, s2.len);
+	}
+	String&operator=(const String& another)
+	{
+		if (this != &another)
+		{
+			String temp(another);
+			this->Swap(temp);
+		}
 		return *this;
 	}
 	friend istream& operator>>(istream& is, String& s1);
@@ -123,25 +164,27 @@ private:
 };
 istream& operator>>(istream& is, String& s1)
 {
-	is >> s1.str;
+	//is >> s1.str;
+	char arr[100] = { 0 };
+	is >> arr;
 	int i = 0;
 	char* temp = s1.str;
-	while (*temp != '\n')
+	for (i = 0; i < 100; i++)
 	{
-		temp++;
-		i++;
-	}
+		if (arr[i] == '\0')
+			break;
+	}                                                                                     
 	if (s1.capacity >= i)
 	{
-
+		s1.len = i;
+		strncpy(s1.str, arr, i);
 	}
 	else
 	{
-		delete[] s1.str;
-		s1.str = NULL;
+		s1.str = (char*)malloc(i*2);
 		s1.len = i;
-		s1.str = new char[s1.len * 2 + 1];
-		s1.capacity = s1.len * 2 + 1;
+		s1.capacity = i*2;
+		strncpy(s1.str, arr, i);
 	}
 	return is;
 }
@@ -157,7 +200,24 @@ ostream& operator<<(ostream& os, String& s1)
 
 int main()
 {
-	String s1("abcd");	String s2(s1);	String s3;	s1 += "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";	s3 += "fhcvbvcb";	cout << s1 << s2<<s3;
+	String s1;
+	cin >> s1; 
+	cout << s1;
+	String s2;
+	s2 = s1;
+	cout << s2;
+	String s3("dasfs");
+	s3 += "xvvxfsdffffffffffffffffff";
+	cout << s3;
+	s3 += 'a';
+	cout << s3;
+	s3 += s2;
+	cout << s3;
+
+
+
+	//String s1("abcd");	//String s2(s1);	//String s3;	//s1 += "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";	//s3 += "fhcvbvcb";	//cout << s1 << s2<<s3;
+
 	system("pause");
 	return 0;
 }
