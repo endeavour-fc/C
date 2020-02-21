a479ff65b940b5b3028d3879be578d6485da2fde
fengchao <1669128906@qq.com>
2020-02-21 17:15:03 +0800

    "相似的树"

--- work_2_12/work_2_12/delete_or_new.cpp
+++ work_2_12/work_2_12/delete_or_new.cpp
@@ -97,6 +97,67 @@ int diameterOfBinaryTree(struct TreeNode* root) {
 		return m2;
 	}
 }
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     struct TreeNode *left;
+*     struct TreeNode *right;
+* };
+*/
+int i = 0;
+bool find(struct TreeNode* root, int k, int temp)
+{
+	if (root == NULL)
+		return false;
+	if (k == temp + root->val)
+		return true;
+	if (find(root->left, k, temp))
+		return true;
+	return find(root->right, k, temp);
+}
+
+bool findTarget(struct TreeNode* root, int k) {
+	if (root == NULL)
+		return false;
+	if (root == 1 && k == 2)
+		return false;
+	if (find(root, k, root->val))
+		return true;
+	if (findTarget(root->left, k))
+		return true;
+	return findTarget(root->right, k);
+}
+void leaf(struct TreeNode* root, int* arr, int* len)
+{
+	if (root == NULL)
+		return;
+	if (root->left == NULL && root->right == NULL)
+	{
+		arr[(*len)++] = root->val;
+	}
+	leaf(root->left, arr, len);
+	leaf(root->right, arr, len);
+}
+
+bool leafSimilar(struct TreeNode* root1, struct TreeNode* root2) {
+	int* arr1 = (int*)malloc(sizeof(int) * 80);
+	int* arr2 = (int*)malloc(sizeof(int) * 80);
+	int len1 = 0;
+	int len2 = 0;
+	int flag = 0;
+	leaf(root1, arr1, &len1);
+	leaf(root2, arr2, &len2);
+	if (len1 != len2)
+		return false;
+	for (int i = 0; i<len1; i++)
+	{
+		if (arr1[i] == arr2[i]);
+		else
+			return false;
+	}
+	return true;
+}
 using namespace std;
 class test
 {
