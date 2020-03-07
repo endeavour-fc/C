01d44159bf20eb2eccdcb2826bf72333fdef5f36
fengchao <1669128906@qq.com>
2020-03-07 18:28:25 +0800

    "字符串转换为数字"

--- work_3_3/work_3_3/work.cpp
+++ work_3_3/work_3_3/work.cpp
@@ -17,6 +17,42 @@ void getstring(string::iterator it1, string::iterator it2)
 		--it2;
 	}
 }
+int StrToInt(string str) {
+	long long sum = 0;
+	int flag = 0;
+	string::iterator it1 = str.begin();
+	if (*it1 == '-')
+		flag = 1;
+	else if (*it1 == '+')
+		flag = 0;
+	else
+		return 0;
+	it1++;
+	while (it1 != str.end())
+	{
+		if (*it1<'0' || *it1>'9')
+			return 0;
+		sum = sum * 10 + (*it1 - '0');
+		if (sum>2147483648 && flag)
+			return 0;
+		else if (sum>2147483647 && !flag)
+			return 0;
+		it1++;
+
+	}
+	if (flag)
+		return 0 - sum;
+	return sum;
+}
+int main()
+{
+	//int a = 0123;
+	//int b = 123;
+	//printf("%o %o\n", a, b);
+	printf("%d\n", StrToInt("-2147483648"));
+	system("pause");
+	return 0;
+}
 //int main()
 //{
 //	vector<int> arr;
@@ -46,63 +82,68 @@ void getstring(string::iterator it1, string::iterator it2)
 //	cout << s1 << endl;
 //	return 0;
 //}
-
-int MoreThanHalfNum_Solution(vector<int> numbers) {
-	sort(numbers.begin(), numbers.end());
-	int temp = numbers[(numbers.size() / 2)];
-	vector<int>::iterator it1 = numbers.begin();
-	vector<int>::iterator it2 = numbers.end() - 1;
-	while (*it1 != temp)
-	{
-		++it1;
-	}
-	while (*it2 != temp)
-	{
-		--it2;
-	}
-	if (it2 - it1 < (numbers.size()) / 2)
-		return 0;
-	return temp;
-}
-
-int fib(int n)
-{
-	count1++;
-	if (n == 0)
-		return 1;
-	if (n == 1)
-		return 2;
-	return fib(n - 1) + fib(n - 2);
-}
-int main()
-{
-	string s1, s2;
-	getline(std::cin, s1);
-	getline(std::cin, s2);
-	int sum = 0;
-	string::iterator it1 = s1.begin();
-
-	while (it1 <= s1.end())
-	{
-		s1.insert(it1, s2.begin(), s2.end());
-		string::iterator right = s1.end() - 1;
-		string::iterator left = s1.begin();
-		while (left<right&&*left == *right)
-		{
-			left++;
-			right--;
-		}
-		if (left >= right)
-			++sum;
-		it1 = s1.erase(it1, it1 + s2.size());
-		if (it1 == s1.end())
-			break;
-		++it1;
-	}
-	cout << sum << endl;
-	system("pause");
-	return 0;
-}
+//char min(char a, char b)
+//{
+//	if (a < b)
+//		return a;
+//	return b;
+//}
+//int MoreThanHalfNum_Solution(vector<int> numbers) {
+//	sort(numbers.begin(), numbers.end());
+//	int temp = numbers[(numbers.size() / 2)];
+//	vector<int>::iterator it1 = numbers.begin();
+//	vector<int>::iterator it2 = numbers.end() - 1;
+//	while (*it1 != temp)
+//	{
+//		++it1;
+//	}
+//	while (*it2 != temp)
+//	{
+//		--it2;
+//	}
+//	if (it2 - it1 < (numbers.size()) / 2)
+//		return 0;
+//	return temp;
+//}
+//
+//int fib(int n)
+//{
+//	count1++;
+//	if (n == 0)
+//		return 1;
+//	if (n == 1)
+//		return 2;
+//	return fib(n - 1) + fib(n - 2);
+//}
+//int main()
+//{
+//	string s1, s2;
+//	getline(std::cin, s1);
+//	getline(std::cin, s2);
+//	int sum = 0;
+//	string::iterator it1 = s1.begin();
+//
+//	while (it1 <= s1.end())
+//	{
+//		s1.insert(it1, s2.begin(), s2.end());
+//		string::iterator right = s1.end() - 1;
+//		string::iterator left = s1.begin();
+//		while (left<right&&*left == *right)
+//		{
+//			left++;
+//			right--;
+//		}
+//		if (left >= right)
+//			++sum;
+//		it1 = s1.erase(it1, it1 + s2.size());
+//		if (it1 == s1.end())
+//			break;
+//		++it1;
+//	}
+//	cout << sum << endl;
+//	system("pause");
+//	return 0;
+//}
 //int main()
 //{
 //	int x = 1;
