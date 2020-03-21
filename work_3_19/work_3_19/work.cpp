b13903ef4f3cffca67a9900338aa3609d119fd6c
fengchao <1669128906@qq.com>
2020-03-21 17:58:54 +0800

    "阶乘数零的个数"

--- work_3_19/work_3_19/work.cpp
+++ work_3_19/work_3_19/work.cpp
@@ -277,6 +277,20 @@ TreeNode* sortedArrayToBST(vector<int>& nums) {
 //
 //	return 0;
 //}
+int trailingZeroes(int n) {
+	if (n<5)
+		return 0;
+	int count = 0;
+	while (n>0)
+	{
+		count += n / 5;
+		n /= 5;
+	}
+	return count;
+
+}
+
+
 int main()
 {
 	string s1;
