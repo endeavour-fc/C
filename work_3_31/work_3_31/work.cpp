d7916aa987549c0ec8209d377ec7cdffe839aabc
fengchao <1669128906@qq.com>
2020-04-11 20:55:19 +0800

    "重复数（极简）"

--- work_3_31/work_3_31/work.cpp
+++ work_3_31/work_3_31/work.cpp
@@ -553,6 +553,15 @@ int integerBreak(int n) {
 	}
 	return v[n];
 }
+int findDuplicate(vector<int>& nums) 
+{
+	while (1)
+	{
+		if (nums[0] == nums[nums[0]])
+			return nums[0];
+		swap(nums[0], nums[nums[0]]);
+	}
+}
 int main()
 {
 	//vector<int> v = { 1,2,5};
