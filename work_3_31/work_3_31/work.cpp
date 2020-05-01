b9722aacf68825ece1dc0adf4f10d1c9534652e0
fengchao <1669128906@qq.com>
2020-05-01 22:24:31 +0800

    "动态规划"

--- work_3_31/work_3_31/work.cpp
+++ work_3_31/work_3_31/work.cpp
@@ -1090,9 +1090,48 @@ int countSubstrings(string s) {
 	}
 	return ret;
 }
+int findLength(vector<int>& A, vector<int>& B) {
+	vector<vector<int>> v(A.size(), vector<int>(B.size(), 0));
+	int ret = 0;
+	for (int i = 0; i<A.size(); i++)
+	{
+		for (int j = 0; j<B.size(); j++)
+		{
+			if (A[i] == B[j])
+			{
+				if (i == 0 || j == 0)
+					v[i][j] = 1;
+				else
+					v[i][j] = v[i - 1][j - 1] + 1;
+				if (ret<v[i][j])
+					ret = v[i][j];
+			}
+		}
+	}
+	return ret;
+}
+int longestCommonSubsequence(string text1, string text2) {
+	vector<vector<int>> v(text1.size() + 1, vector<int>(text2.size() + 1, 0));
+	for (int i = 1; i <= text1.size(); i++)
+	{
+		for (int j = 1; j <= text2.size(); j++)
+		{
+			if (text1[i - 1] == text2[j - 1])
+			{
+				v[i][j] = v[i - 1][j - 1]+1;
+			}
+			else
+			{
+				v[i][j] = max(v[i - 1][j], v[i][j - 1]);
+			}
+		}
+	}
+	return v[text1.size()][text2.size()];
+}
 int main()
 {
-	countSubstrings("aaa");
+	cout << longestCommonSubsequence("abcde", "ace");
+	//countSubstrings("aaa");
 	//vector<int> v1 = { 1,2,3,4 };
 	//vector<int> v2 = { 5,6,7,8 };
 	//vector<vector<int>> v = { {0,1,1,1},{1,1,1,1},{0,1,1,1 } };
