e388a6492a3d4c280d1f2dd2de9ac37e49dd2d99
fengchao <1669128906@qq.com>
2020-05-03 21:57:47 +0800

    "二分"

--- work_3_31/work_3_31/work.cpp
+++ work_3_31/work_3_31/work.cpp
@@ -1178,6 +1178,28 @@ bool palindrome(string s)
 	}
 	return true;
 }
+int guessNumber(int n) {
+	int left = 1;
+	int right = n;
+
+	while (left <= right)
+	{
+		unsigned int mid = left + (right - left) / 2;
+		if (guess(mid) == 1)
+		{
+			left = mid + 1;
+		}
+		else if (guess(mid) == -1)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return 0;
+}
 int main()
 {
 	cout << longestCommonSubsequence("abcde", "ace");
