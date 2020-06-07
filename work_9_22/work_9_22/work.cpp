147bb006f59e1eabc4c7b48a200fd70376878013
fengchao <1669128906@qq.com>
2020-06-07 21:39:35 +0800

    "最大空间"

--- work_9_22/work_9_22/work.cpp
+++ work_9_22/work_9_22/work.cpp
@@ -1,7 +1,9 @@
 #define  _CRT_SECURE_NO_WARNINGS 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<iostream>
+#include<string>
+#include <algorithm>
+#include<vector>
+using namespace std;
 
 int findnum(int arr[], int len)
 {
@@ -31,13 +33,73 @@ int findnum(int arr[], int len)
 }
 
 
+//int main()
+//{
+//	int arr[] = { 1,1,1,2,2,2,3,3,3,4,4,4,5,6,6,6 };
+//	int len = sizeof(arr) / sizeof(arr[0]);
+//	
+//	printf("%d\n", findnum(arr, len));
+//
+//	system("pause");
+//	return 0;
+//}
+bool myfun(string s1, string s2)
+{
+	return s1.size() < s2.size();
+}
 int main()
 {
-	int arr[] = { 1,1,1,2,2,2,3,3,3,4,4,4,5,6,6,6 };
-	int len = sizeof(arr) / sizeof(arr[0]);
-	
-	printf("%d\n", findnum(arr, len));
+	int num = 0;
+	while (cin >> num)
+	{
+		vector<string> ret;
+		string temp;
+		for (int i = 0; i<num; ++i)
+		{
+			cin >> temp;
+			ret.push_back(temp);
+		}
+		sort(ret.begin(), ret.end(), myfun);
+		vector<string>::iterator it1 = ret.begin()+1;
+		while (it1 != ret.end())
+		{
+			if ((*it1).find((*(it1 - 1))) != string::npos)
+				it1=ret.erase(it1 - 1);
+			it1++;
+		}
 
-	system("pause");
+		for (int i = 0; i < ret.size(); ++i)
+		{
+			cout <<"mkdir -p "<< ret[i] << endl;
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
+
+int main01()
+{
+	string s;
+	int name;
+	int num;
+	while (cin >> num)
+	{
+		int temp = 0;
+		int ret = 0;
+		for (int i = 0; i<num; ++i)
+		{
+			cin.get();
+			getline(cin, s);
+			if (s.find("disconnect") !=string::npos)
+				--temp;
+			else
+				++temp;
+			ret = (ret>temp ? ret : temp);
+		}
+		cout << ret << endl;
+	}
+
 	return 0;
 }
