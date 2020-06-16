ec774b1a12df0a1c0aa603b6df5d9d4da3c6776c
fengchao <1669128906@qq.com>
2020-06-16 22:25:07 +0800

    "字符串中出现的第一个唯一字符"

--- leetcode/work_3_31/work.cpp
+++ leetcode/work_3_31/work.cpp
@@ -1918,6 +1918,18 @@ int lengthOfLongestSubstring(string s) {
 	}
 	return ret;
 }
+class Solution_firstUniqChar {
+public:
+	char firstUniqChar(string s) {
+		map<char, int> mp;
+		for (int i = 0; i<s.size(); ++i)
+			++mp[s[i]];
+		for (int i = 0; i<s.size(); ++i)
+			if (mp[s[i]] == 1)
+				return s[i];
+		return ' ';
+	}
+};
 int main()
 {
 	cout << lengthOfLongestSubstring("abccdefa") << endl;
