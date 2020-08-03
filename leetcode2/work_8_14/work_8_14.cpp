633d61250705481b1f7f98d7f750e6ec7444828c
fengchao <1669128906@qq.com>
2020-08-03 17:24:36 +0800

    "编辑距离"

--- leetcode2/work_8_14/work_8_14.cpp
+++ leetcode2/work_8_14/work_8_14.cpp
@@ -392,6 +392,27 @@ public:
 		return grid[temp.first][temp.second];
 	}
 };
+class Solution {
+public:
+	int minDistance(string word1, string word2) {
+		vector<vector<int>> v(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+		for (int i = 0; i <= word1.size(); ++i)
+			v[i][0] = i;
+		for (int i = 0; i <= word2.size(); ++i)
+			v[0][i] = i;
+
+		for (int i = 1; i <= word1.size(); ++i)
+		{
+			for (int j = 1; j <= word2.size(); ++j)
+			{
+				v[i][j] = min(v[i - 1][j - 1], min(v[i][j - 1], v[i - 1][j])) + 1;
+				if (word1[i - 1] == word2[j - 1])
+					v[i][j] = min(v[i][j], v[i - 1][j - 1]);
+			}
+		}
+		return v[word1.size()][word2.size()];
+	}
+};
 int main()
 {
 	vector<vector<int>> v = { {1, 0, 1},{0, 0, 0},{1, 0, 1 } };
