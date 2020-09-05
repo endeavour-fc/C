af15d97512ef545d226911a32aafa25697e7d6d7
fengchao <1669128906@qq.com>
2020-09-05 20:45:12 +0800

    "搜狗笔试"

--- work_9_3/work_9_3/work.cpp
+++ work_9_3/work_9_3/work.cpp
@@ -5,6 +5,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<unordered_map>
 using namespace std;
 typedef struct Node
 {
@@ -79,18 +80,48 @@ public:
 		return ret;
 	}
 };
+
+unordered_map<string, int> mp;
+void get(string& password, int pos, string ret)
+{
+	if (ret.size() == password.size())
+	{
+		++mp[ret];
+		return;
+	}
+	if ((password[pos] - '0' + ret[ret.size() - 1] - '0') % 2)
+	{
+		ret += (password[pos] - '0' + ret[ret.size() - 1] - '0') / 2 + 1+'0';
+		get(password, pos + 1, ret);
+		ret.pop_back();
+	}
+	ret += (password[pos] - '0' + ret[ret.size() - 1] - '0') / 2+'0';
+	get(password, pos + 1, ret);
+}
+long long getPasswordCount(string password) {
+	// write code here
+
+	for (int i = 0; i<10; ++i)
+	{
+		string temp;
+		temp += ('0' + i);
+		get(password, 1, temp);
+	}
+	return mp.size() - mp.count(password);
+}
 int main()
 {
-	Node* head = NULL;
-	int input = 0;
-	do
-	{
-		printf("请输入一个非-1的数\n");
-		scanf("%d", &input);
-		creat_node(&head, input);
-	} while (input!=-1);
-	
-	print_node(head);
+	cout << getPasswordCount("123") << endl;
+	//Node* head = NULL;
+	//int input = 0;
+	//do
+	//{
+	//	printf("请输入一个非-1的数\n");
+	//	scanf("%d", &input);
+	//	creat_node(&head, input);
+	//} while (input!=-1);
+	//
+	//print_node(head);
 
 	system("pause");
 	return 0;
